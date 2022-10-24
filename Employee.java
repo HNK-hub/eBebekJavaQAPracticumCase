7c29975d57347f8af28d2bae2059e4226536bee4
Hatice Nur KAYA <66225075+HNK-hub@users.noreply.github.com>
2022-10-25 00:04:00 +0300

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,11 +1,11 @@
 
 public class Employee {
-    //Istenilen 4 nitelik
+    // Istenilen 4 nitelik
     private String name;
-    private double salary; 
+    private double salary;
     private int workHours;
     private int hireYear;
- 
+
     // Constructor: Kurucu metot
     // Employee(name,salary,workHours,hireYear) : Kurucu metot olup 4 parametre
     // alacaktır.
@@ -15,7 +15,9 @@ public class Employee {
         this.workHours = workHours;
         this.hireYear = hireYear;
     }
-    //Getter Setter diger sınıfta  okuma ve yazma işlemini yapabilmek için kullanıldı.
+
+    // Getter Setter diger sınıfta okuma ve yazma işlemini yapabilmek için
+    // kullanıldı.
     public String getName() {
         return this.name;
     }
@@ -47,58 +49,58 @@ public class Employee {
     public void setHireYear(int hireYear) {
         this.hireYear = hireYear;
     }
+
     double a;
-    public double tax(){
-        if(salary >0  && salary < 1000  ) 
+
+    public double tax() {
+        if (salary > 0 && salary < 1000)
             return 0.0;
-        else{
-            a =salary * 0.03;
+        else {
+            a = salary * 0.03;
             return a;
         }
     }
 
     public double bonus() {
-        
+
         if (workHours <= 40) {
-            a=0;
+            a = 0;
             return a;
-        }
-        else if (workHours > 40) {
+        } else if (workHours > 40) {
             workHours -= 40;
             a = workHours * 30;
             return a;
-        }else
+        } else
             return 0;
-    } 
+    }
+
     public double raiseSalary() {
-         int year =2021;
-         int workYear = year - hireYear;
+        int year = 2021;
+        int workYear = year - hireYear;
+        
         if (workYear < 10 && workYear >= 0) {
             a = salary * 0.05;
-			return a;
-		} else if (workYear > 9 && workYear < 20) {
+            return a;
+        } else if (workYear > 9 && workYear < 20) {
             a = salary * 0.1;
-			return a;
-		} else if (workYear > 19) {
+            return a;
+        } else if (workYear > 19) {
             a = salary * 0.15;
-			return a;
-		} 
-        double taxBonus =(a + salary +bonus()-tax());
-        double newSalary =(a + salary +bonus());
-		System.out.println("Vergi ve Bonuslar ile birlikte maaş: " + taxBonus);
-        System.out.println("Toplam Maaş: " + newSalary);
-        return  a;
+            return a;
+        }
+        return a;
     }
 
     public String toString() {
         System.out.println("Adı: " + getName());
-		System.out.println("Maaşı: " + getSalary());
-		System.out.println("Çalışma Saati: " + getWorkHours());
-		System.out.println("Başlangıç Yılı: " + getHireYear());
-		System.out.println("Vergi: " + tax());
-		System.out.println("Bonus: " + bonus());
-		System.out.println("Maaş artışı: " + raiseSalary());
-
+        System.out.println("Maaşı: " + getSalary());
+        System.out.println("Çalışma Saati: " + getWorkHours());
+        System.out.println("Başlangıç Yılı: " + getHireYear());
+        System.out.println("Vergi: " + tax());
+        System.out.println("Bonus: " + bonus());
+        System.out.println("Maaş artışı: " + raiseSalary());
+        System.out.println("Vergi ve Bonuslar ile birlikte maaş: " +  (salary + bonus() - tax()));
+        System.out.println("Toplam Maaş: " +  ((salary + bonus()+ raiseSalary())-tax() ));
         return "Employee Name: " + this.name + " Salary: " + this.salary + " Work hours: " + this.workHours
                 + " Hire Year: " + this.hireYear;
     }
